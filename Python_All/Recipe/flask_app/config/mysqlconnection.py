57733163b9acee9c16fe6a8ef19d823f253518b9
Truman Tang <39065851+doomphd@users.noreply.github.com>
2023-05-19 08:24:00 -0700

    Update mysqlconnection.py

--- Python_All/Recipe/flask_app/config/mysqlconnection.py
+++ Python_All/Recipe/flask_app/config/mysqlconnection.py
@@ -6,7 +6,7 @@ class MySQLConnection:
         # change the user and password as needed
         connection = pymysql.connect(host = 'localhost',
                                     user = 'root', 
-                                    password = '4576478Tt!', 
+                                    password = 'x', 
                                     db = db,
                                     charset = 'utf8mb4',
                                     cursorclass = pymysql.cursors.DictCursor,
